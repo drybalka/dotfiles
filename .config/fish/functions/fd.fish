ab62c9e453bf7f289b80cb14ba6e8716e6990ada
Denys Rybalka <denys.rybalka@tuta.io>
2026-01-16 22:55:28 +0100

    fix: fd and metals

--- .config/fish/functions/fd.fish
+++ .config/fish/functions/fd.fish
@@ -1,4 +1,4 @@
 function fd --wraps fd --description 'Better find files with fd and eza'
-    command fd --hidden --no-ignore --list-details $argv \
+    command fd --hidden --no-ignore $argv \
         --exec-batch eza --group-directories-first --sort=extension --git --long --smart-group
 end
